51d1f47bae0f9e101fcd05afba8d6fac39922f01
Dmitry Votchitsev <dmitry.votchitsev@gmail.com>
2021-10-20 19:33:35 +0200

    change task two

---
+++ task2.py
@@ -0,0 +1,17 @@
+import os
+import hashlib
+
+path = os.path.abspath('countries.json')
+
+
+def my_generator(p):
+    file = open(p, 'r')
+    cursor = 0
+    num_of_strings = len(file.readlines())
+    while cursor <= num_of_strings:
+        result = hashlib.md5(file.readline().encode('utf-8')).hexdigest()
+        yield result
+        cursor += 1
+
+
+print(list(my_generator(path)))
